23d34aef033de4e7a795360ac98340a062906c93
exploratoryprorammer <rohan.k.sah@gmail.com>
2026-06-06 15:53:43 -0500

    feat: postgres + clerk lambda

---
+++ backend/cmd/api/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/clerk/clerk-sdk-go/v2"
+
+	"kronos/internal/api"
+	"kronos/internal/config"
+	"kronos/internal/store"
+)
+
+func main() {
+	ctx := context.Background()
+
+	clerk.SetKey(config.Get(ctx, "CLERK_SECRET_KEY"))
+
+	db, err := store.NewPostgres(ctx, config.Get(ctx, "DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	handler := &api.API{Store: db}
+	lambda.Start(handler.Handle)
+}
