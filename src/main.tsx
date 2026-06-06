1c2e96b7ce5bf238212de2fa0018741b0a8ac7b2
exploratoryprorammer <rohan.k.sah@gmail.com>
2026-06-06 16:00:11 -0500

    app + main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-react";
+import "./index.css";
+import App from "./App";
+import { CLERK_KEY, useClerk } from "./lib/env";
+import { DataProvider } from "./data/source";
+import { SignInScreen } from "./auth/SignInScreen";
+import { AuthGate } from "./auth/AuthGate";
 
-createRoot(document.getElementById('root')!).render(
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    {useClerk && CLERK_KEY ? (
+      <ClerkProvider publishableKey={CLERK_KEY}>
+        <SignedOut>
+          <SignInScreen />
+        </SignedOut>
+        <SignedIn>
+          <AuthGate />
+        </SignedIn>
+      </ClerkProvider>
+    ) : (
+      <DataProvider getToken={async () => null}>
+        <App />
+      </DataProvider>
+    )}
+  </StrictMode>
+);
